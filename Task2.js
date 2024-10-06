869e86c6cf040b048ea132657afcfa36d6b7f8c7
Uday kiran <nagauday7788@gmail.com>
2024-10-06 14:06:42 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,24 @@
+
+// var varaible 
+
+
+var a=25;
+var name="Uday kiran";
+console.log(a);
+console.log(name);
+
+// let varaible 
+
+
+let b=45;
+let hello="Hello Everyone";
+console.log(b);
+console.log(hello);
+
+
+// const varaible 
+const c=65;
+const message="good morning";
+console.log(c);
+console.log(message);
+
---
+++ Task3.js
@@ -0,0 +1,24 @@
+var a=25;
+var name="Uday kiran";
+var value=true;
+var b;
+var empty=null;
+var obj={
+ name:"Abhijeeth",
+ age:24,
+ Adress:"Nizamabad"
+};
+
+var arr=[2,3,4,5,6]
+console.log(typeof a);
+console.log(typeof name);
+console.log(typeof value);
+console.log(typeof b);
+console.log(typeof empty);
+
+
+
+
+
+console.log(typeof arr);
+console.log(typeof obj)
